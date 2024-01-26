11b77255d6aae6232f3f18873cada4fa6ed808b4
snehajhaaa <130882058+snehajhaaa@users.noreply.github.com>
2024-01-26 17:46:49 +0545

    o

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,172 @@
-(() => {
-  "use strict"; const e = (e, s = !1) => { e = e.trim(); return s ? [...document.querySelectorAll(e)] : document.querySelector(e) }, s = (s, t, o, n = !1) => { let l = e(t, n); l && (n ? l.forEach(e => e.addEventListener(s, o)) : l.addEventListener(s, o)) }, t = (e, o) => { e.addEventListener("scroll", o) }; let o = e("#navbar .scrollto", !0), n = () => { let s = window.scrollY + 200; o.forEach(o => { if (!o.hash) return; let n = e(o.hash); if (!n) return; if (s >= n.offsetTop && s <= (n.offsetTop + n.offsetHeight)) o.classList.add("active"); else o.classList.remove("active") }) }; window.addEventListener("load", n), t(document, n); const l = e => { let s = e => { let o = e.offsetHeight; if (!e.classList.contains("header-scrolled")) o -= 20; let n = e.offsetTop; window.scrollTo({ top: n - o, behavior: "smooth" }) }; s("#header"), s("#header") }; let i = e("#header"); i && (s(window, "load", () => { window.scrollY > 100 ? i.classList.add("header-scrolled") : i.classList.remove("header-scrolled") }), t(document, () => { window.scrollY > 100 ? i.classList.add("header-scrolled") : i.classList.remove("header-scrolled") }));
-  let c = e(".back-to-top"); c && (s(window, "load", () => { window.scrollY > 100 ? c.classList.add("active") : c.classList.remove("active") }), t(document, () => { window.scrollY > 100 ? c.classList.add("active") : c.classList.remove("active") }));
-  s("click", ".mobile-nav-toggle", function () { e("#navbar").classList.toggle("navbar-mobile"), this.classList.toggle("bi-list"), this.classList.toggle("bi-x") }), s("click", ".navbar .dropdown > a", function (s) { if (e("#navbar").classList.contains("navbar-mobile")) { s.preventDefault(), this.nextElementSibling.classList.toggle("dropdown-active") } }, !0), s("click", ".scrollto", function (s) { if (e(this.hash)) { s.preventDefault(); let t = e("#navbar"); t.classList.contains("navbar-mobile") && (t.classList.remove("navbar-mobile"), e(".mobile-nav-toggle").classList.toggle("bi-list"), e(".mobile-nav-toggle").classList.toggle("bi-x")), l(this.hash) } }), window.addEventListener("load", () => { window.location.hash && e(window.location.hash) && l(window.location.hash) }); let a = e(".skills-content"); a && new Waypoint({ element: a, offset: "80%", handler: s => { let o = e(".progress .progress-bar", !0); o.forEach(e => { e.style.width = e.getAttribute("aria-valuenow") + "%" }) } })
-})();
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 20
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+})()
